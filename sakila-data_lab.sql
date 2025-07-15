af04cf7a50aad1522919b639d041b50f6244c0ee
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-15 12:25:17 +0200

    Create sakila-data_lab.sql

---
+++ sakila-data_lab.sql
@@ -0,0 +1,11 @@
+USE sakila;
+select * from actor;
+select * from film; 
+select * from customer;
+select film_id from film;
+select language_id FROM language;
+select first_name from staff;
+
+-- Retrieve unique release years.
+select
+
