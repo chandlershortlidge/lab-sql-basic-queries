9ecd4c85fca0e61e4760db92974c0aba401db7f7
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-15 13:16:11 +0200

    Update sakila-data_lab.sql

--- sakila-data_lab.sql
+++ sakila-data_lab.sql
@@ -1,11 +1,17 @@
 USE sakila;
+-- 1. 
+show tables;
+-- 2. 
 select * from actor;
-select * from film; 
+select * from film;
 select * from customer;
-select film_id from film;
-select language_id FROM language;
+
+-- 3. 
+select title from film;
+-- select language_id from language;
 select first_name from staff;
 
--- Retrieve unique release years.
-select
+-- 4. 
+select distinct release_year from film;
+
 
