3b768f65ed3a50b19956f2a231037be7955b4379
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-15 13:49:57 +0200

    Update sakila-data_lab.sql

--- sakila-data_lab.sql
+++ sakila-data_lab.sql
@@ -8,10 +8,37 @@ select * from customer;
 
 -- 3. 
 select title from film;
--- select language_id from language;
+-- select original_language from flim;
 select first_name from staff;
 
 -- 4. 
 select distinct release_year from film;
+
+-- 5.
+select count(*) store_id from store;
+select count(*) staff_id from staff;
+select count(*) inventory_id from inventory; -- 4581
+select count(*) rental_id from rental; -- 16044
+select distinct count(*) last_name from actor; -- 200
+
+-- 6. 
+select title, length
+from film
+order by length desc
+limit 10;
+
+-- 7. 
+select last_name from actor
+where first_name = 'SCARLETT';
+
+-- bonus:
+select * from film
+where title like "%ARMAGEDDON%"
+order by length > 100;
+
+select count(*)
+from film
+where special_features = "Behind the Scenes";
+
 
 
